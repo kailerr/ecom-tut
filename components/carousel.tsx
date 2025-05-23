2c10bd0ca861670a5e3426fbbc541da45e3c9302
kailerr <kapech279@gmail.com>
2025-05-22 17:11:37 -0700

    update to card styling and carousel rotations

--- components/carousel.tsx
+++ components/carousel.tsx
@@ -25,24 +25,28 @@ export const Carousel = ({ products }: Props) => {
   const price = currentProduct.default_price as Stripe.Price;
 
   return (
-    <Card>
-      {currentProduct.images && currentProduct.images[0] && (
-            <div>
-              <Image
-                alt={currentProduct.name}
-                src={currentProduct.images[0]}
-                layout="fill"
-                objectFit="cover"
-              />
-            </div>
-          )
-        }
-        <CardContent>
-            <CardTitle>
-                {currentProduct.name}
-            </CardTitle>
-            {price && price.unit_amount && <p>${(price.unit_amount /100).toFixed(2)}</p>}
-        </CardContent>
-    </Card>
+    <Card className="relative overflow-hidden rounded-lg shadow-md border-gray-300">
+    {currentProduct.images && currentProduct.images[0] && (
+      <div className="relative h-80 w-full">
+        <Image
+          src={currentProduct.images[0]}
+          alt={currentProduct.name}
+          layout="fill"
+          objectFit="cover"
+          className="transition-opacity duration-500 ease-in-out"
+        />
+      </div>
+    )}
+    <CardContent className="absolute inset-0 flex flex-col items-center justify-center bg-black bg-opacity-50">
+      <CardTitle className="text-3xl font-bold text-white mb-2">
+        {currentProduct.name}
+      </CardTitle>
+      {price && price.unit_amount && (
+        <p className="text-xl text-white">
+          ${(price.unit_amount / 100).toFixed(2)}
+        </p>
+      )}
+    </CardContent>
+  </Card>
   );
 };
