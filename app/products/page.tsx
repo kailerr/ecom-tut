7b6e72fbf8c48a9e4c8153f87e632f665f4d176d
kailerr <kapech279@gmail.com>
2025-05-27 18:49:27 -0700

    finished search bar filtering

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,5 +1,16 @@
-export default function ProductsPage() {
+import { ProductList } from "@/components/ui/product-list";
+import { stripe } from "@/lib/stripe";
+
+export default async function ProductsPage() {
+
+  const products = await stripe.products.list({
+    expand: ["data.default_price"],
+
+  });
   return(
-  <div>product</div>
+  <div><h1>All Products</h1>
+  <ProductList products={products.data}/>
+  
+  </div>
   ); 
 }
